06a1333c2bac0e01973eaa4ef70dee9344e2b21c
Gaurav Jain <extensions@magecomp.com>
2022-04-25 12:57:32 +0530

    Release 1.0.1

---
+++ Smsfree/Block/Adminhtml/Collect.php
@@ -0,0 +1,30 @@
+<?php
+namespace Magecomp\Smsfree\Block\Adminhtml;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Collect extends Field
+{
+    protected $_template = 'Magecomp_Smsfree::collect.phtml';
+
+    public function __construct(
+        Context $context,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+    }
+
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+}
